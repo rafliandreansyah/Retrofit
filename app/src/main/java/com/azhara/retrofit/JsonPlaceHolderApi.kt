f54ed00202579b2eadff16387e3157a60222fe80
Rafli Andreansyah <rafliandreansyah957@gmail.com>
2020-03-30 13:59:20 +0700

    pengertian put dan patch dan interface patch pada retrofit

--- app/src/main/java/com/azhara/retrofit/JsonPlaceHolderApi.kt
+++ app/src/main/java/com/azhara/retrofit/JsonPlaceHolderApi.kt
@@ -37,8 +37,19 @@ interface JsonPlaceHolderApi {
     @POST("posts")
     fun createPost(@FieldMap field: Map<String, String>): Call<Post>
 
+
+/**    Perbedaan put dan patch yaitu put digunakan untuk pebaharuan semua data, seumpama data yang diperbaharui title, text
+ *      Salah satunya bernilai null seumpama title bernilai null maka dalam penyimpanan tersebut title bernilai null, berbeda
+ *      dengan patch diperuntukan untuk pembaharuan tidak semua, seumpama title bernilai null atau kosng maka, data title akan megambil
+ *      data dari sebelumnya., gunakan put sebagai pembaharuan yang diperuntukkan untuk semua, dan gunakan patch sebagai pembaharuan
+ *      beberapa.
+**/
 //    Put retrofit
     @PUT("posts/{id}")
     fun putPost(@Path("id") id: Int?)
+
+//    Patch retrofit
+    @PATCH("posts/{id}")
+    fun patchPost(@Path("id") id: Int?)
 
 }
